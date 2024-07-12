d4b6a0f955d0c2c161bc6fee885c76bf57e8ead6
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-07-12 18:08:59 +0100

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,4 +24,4 @@ const IndexPage: React.FC<PageProps> = () => {
 
 export default IndexPage
 
-export const Head: HeadFC = () => <title>Home Page</title>
+export const Head: HeadFC = () => <title>Admin Resources</title>
