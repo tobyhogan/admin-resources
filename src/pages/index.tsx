9bb9ec37b6b751853125e463569d60c1722c1dc5
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-07-26 17:32:35 +0100

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,8 +24,8 @@ const IndexPage: React.FC<PageProps> = () => {
         <li>- <a href="https://tobyhogan.github.io/tsx-gatsby-tailwind-landing-template/" target="_blank">JSX Gatsby Tailwind Landing Page</a></li>
       </ul>
       <h3 className="mt-6">Links</h3>
-      <ul className="mt-3 pl-8">
-        
+      <ul className="mt-3 pl-6">
+
         <li>- <a href="https://tobyhogan.space" target="_blank">(tobyhogan.space) Site</a></li>
         <li>- <a href="https://kazenprojects.com" target="_blank">Kazen Projects Landing Page</a></li>
         <li>- <a href="https://habit-tracker-indol-ten.vercel.app/habit" target="_blank">Habit Tracker</a></li>
