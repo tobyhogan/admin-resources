83363f690d8ee610f560bf7633be857b2f6655ea
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-07-26 17:31:31 +0100

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,6 +25,7 @@ const IndexPage: React.FC<PageProps> = () => {
       </ul>
       <h3 className="mt-6">Links</h3>
       <ul className="mt-3 pl-8">
+        
         <li>- <a href="https://tobyhogan.space" target="_blank">(tobyhogan.space) Site</a></li>
         <li>- <a href="https://kazenprojects.com" target="_blank">Kazen Projects Landing Page</a></li>
         <li>- <a href="https://habit-tracker-indol-ten.vercel.app/habit" target="_blank">Habit Tracker</a></li>
