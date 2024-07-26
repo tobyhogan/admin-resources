a4ee2fee200c9581fef36db1c8811f96ba5a0044
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-07-26 11:43:46 +0100

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,9 +14,14 @@ const mainStyles = {
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <main className="">
-      <h1 className="text-center text-3xl mt-16 underline">Admin Resources</h1>
-      <ul className="mx-auto w-fit mt-6">
+      <h1 className="text-center text-3xl mt-16 font-extrabold"> &#62;&#62; ADMIN RESOURCES &#60;&#60;</h1>
+      <h3 className="mt-16">Statistics:</h3>
+      <ul className="mt-3">
         <li>- <a href="https://tobyhogan.github.io/metrics-dashboard/" target="_blank">Habitazen: Metrics Tracker</a></li>
+      </ul>
+      <h3 className="mt-6">Templates</h3>
+      <ul className="mt-3">
+        <li>- <a href="https://tobyhogan.github.io/tsx-gatsby-tailwind-landing-template/" target="_blank">JSX Gatsby Tailwind Landing Page</a></li>
       </ul>
     </main>
   )
