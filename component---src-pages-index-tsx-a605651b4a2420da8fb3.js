7d093ae774e7ccf1a48c8d43fd47b897aed7b538
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-07-27 21:12:33 +0100

    Updates

---
+++ component---src-pages-index-tsx-a605651b4a2420da8fb3.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktobyhogan_space=self.webpackChunktobyhogan_space||[]).push([[245],{5823:function(e,t,a){a.r(t),a.d(t,{Head:function(){return l}});var n=a(758);t.default=()=>n.createElement("main",{className:""},n.createElement("h1",{className:"text-5xl mx-auto w-fit bg-blue-500 mt-10 py-2 px-4"},"TSX. Gatsby. Tailwind."));const l=()=>n.createElement("title",null,"Home Page")}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-a605651b4a2420da8fb3.js.map
