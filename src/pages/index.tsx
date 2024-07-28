f52016748be673b1cfa814d439dff1b015420706
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-07-28 15:58:20 +0100

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,14 +24,14 @@ const IndexPage: React.FC<PageProps> = () => {
         <li>- <a href="https://tobyhogan.github.io/tsx-gatsby-tailwind-landing-template/" target="_blank">JSX Gatsby Tailwind Landing Page</a></li>
       </ul>
       <h3 className="mt-6">Business Links</h3>
-      <ul className="mt-3 pl-9">
+      <ul className="mt-3 pl-4">
         <li>- <a href="https://habit-tracker-indol-ten.vercel.app/habit" target="_blank">Habit Tracker</a></li>
         <li>- <a href="https://tobyhogan.github.io/habit-tracker-landing-page/about" target="_blank">Habit Tracker Landing Page</a></li>
         <li>- <a href="https://kazenprojects.com" target="_blank">Kazen Projects Landing Page</a></li>
 
       </ul>
       <h3 className="mt-6">Other Links</h3>
-      <ul className="mt-3 pl-9">
+      <ul className="mt-3 pl-4">
         <li>- <a href="https://tobyhogan.space" target="_blank">TobyHogan.space Site</a></li>
         <li>- <a href="https://browser-music-viewer-9s0daum6l-toby-hogans-projects.vercel.app/" target="_blank">Track Stem Mixer Web App</a></li>
 
